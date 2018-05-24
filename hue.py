0d97d554b7b36e7116b6d8590d839f3e8f7dee9e
Michael Brunner <MichaelBrunn3r@gmail.com>
2018-05-24 10:15:27 +0200

    Simplified 'hue.py'
    Removed HueLightState and HueLightCapabilities. All Light control is now inside the HueLight class. Added Parent class HueApiObject (automatically adds attributes from json to Api Objects)

--- hue.py
+++ hue.py
@@ -1,124 +1,86 @@
 import json
 import time
-import re
 import requests
+from collections import deque
+import hue_color
 
 HUE_DEFAULT_TRANSITION_TIME = 4
-HUE_LIGHT_TYPES = {"Color light":{"supports_color":True, "supports_ct":False, "isDimmable":True}, \
-                    "Extended color light":{"supports_color":True, "supports_ct":True, "isDimmable":True}, \
-                    "Color temperature light":{"supports_color":False, "supports_ct":True, "isDimmable":True}, \
-                    "Dimmable light":{"supports_color":False, "supports_ct":False, "isDimmable":False}}
+HUE_LIGHT_TYPES = {"Color light":{"supports_color":True, "supports_ct":False, "dimmable":True}, \
+                    "Extended color light":{"supports_color":True, "supports_ct":True, "dimmable":True}, \
+                    "Color temperature light":{"supports_color":False, "supports_ct":True, "dimmable":True}, \
+                    "Dimmable light":{"supports_color":False, "supports_ct":False, "dimmable":True}}
+HUE_DEFAULT_USERNAME = "newdeveloper"
 
-class HueLightCapabilites:
-    def __init__(self, light_type, capabilities):
-        light_type = light_type.lower()
+class HueBridge(object):
+    def __init__(self, ip, username=HUE_DEFAULT_USERNAME):
+        self.ip = ip
+        self.username = username
+        self.base_url = 'http://' + self.ip + '/api/' + username
+        self.lights = self.get_lights()
+        self.sensors = self.get_sensors()
 
-        for type_name, type_capabilities in HUE_LIGHT_TYPES.items():
-            if light_type == type_name.lower():
-                self.supports_color = type_capabilities['supports_color']
-                self.supports_ct = type_capabilities['supports_ct']
-                self.isDimmable = type_capabilities['isDimmable']
-                break
+    def _request(self, method, relative_url, data={}):
+        if method == 'GET': return requests.get(self.base_url + relative_url)
+        elif method == 'PUT': return requests.put(self.base_url + relative_url, json.dumps(data))
 
-        if self.supports_color: self.colorgamut = capabilities['control']['colorgamut']
-        if self.supports_ct: self.ct = capabilities['control']['ct']
-        if self.isDimmable: self.mindimlevel = capabilities['control']['mindimlevel']
+    def get_lights(self):
+        response = self._request('GET', HueLight.ROUTE).json()
 
-    def __str__(self):
-        attr = {}
-        if hasattr(self, 'colorgamut'): attr['colorgamut'] = self.colorgamut
-        if hasattr(self, 'ct'): attr['ct'] = self.ct
-        if hasattr(self, 'mindimlevel'): attr['mindimlevel'] = self.mindimlevel
-        return json.dumps(attr)
+        lights = dict()
+        for id, attr in response.items():
+            lights[id] = HueLight(self, id, attr)
 
+        return lights
 
-class HueLightState:
-    def __init__(self, on, state):
-        self.on = on
+    def get_sensors(self):
+        response = self._request('GET', HueSensor.ROUTE).json()
+        
+        sensors = dict()
+        for id, attr in response.items():
+            sensors[id] = HueSensor(self, id, attr)
 
-        bri = state.get('bri')
-        if bri != None: self.bri = bri
+        return sensors
 
-        ct = state.get('ct')
-        if ct != None: self.ct = ct
+class HueApiObject:
+    def __init__(self, bridge, id, attr):
+        object.__setattr__(self, 'bridge', bridge)
+        object.__setattr__(self, 'id', id)
 
-        hue = state.get('hue')
-        if hue != None: self.hue = hue
-
-        sat = state.get('sat')
-        if sat != None: self.sat = sat
-
-    def __str__(self):
-        attr = {}
-        attr['on'] = self.on
-        if hasattr(self, 'bri'): attr['bri'] = self.bri
-        if hasattr(self, 'ct'): attr['ct'] = self.ct
-        if hasattr(self, 'hue'): attr['hue'] = self.hue
-        if hasattr(self, 'sat'): attr['sat'] = self.sat
-        return json.dumps(attr)
-
-
-class HueLight:
-    def __init__(self, id, uniqueid, modelid, name, light_type, capabilities, state):
-        self.id = int(id)
-        self.uniqueid = uniqueid
-        self.modelid = modelid
-        self.name = name
-        self.type = light_type
-
-        # Capabilites of the light
-        self.capabilities = capabilities
-
-        # State of the light
-        self.state = state
-
-    def on(self, transitiontime=HUE_DEFAULT_TRANSITION_TIME):
-        requests.put(base_url + "/lights/" + str(self.id) + "/state", data=json.dumps({'on':True, 'transitiontime':transitiontime, 'bri':self.state.bri}))
-        self.state.on = True
-
-    def off(self, transitiontime=HUE_DEFAULT_TRANSITION_TIME):
-        requests.put(base_url + "/lights/" + str(self.id) + "/state", data=json.dumps({'on':False, 'transitiontime':transitiontime}))
-        self.state.on = False
-
-    def blink(self):
-        if self.state.on:
-            self.off(0)
-            time.sleep(1)
-            self.on(0)
-        else:
-            self.on(0)
-            time.sleep(1)
-            self.off(0)
+        for attr_name, val in attr.items():
+            object.__setattr__(self, attr_name, val)
 
     def __str__(self):
-        attr = {}
-        attr['id'] = self.id
-        attr['uniqueid'] = self.uniqueid
-        attr['modelid'] = self.modelid
-        attr['name'] = self.name
-        attr['state'] = json.loads(str(self.state))
-        attr['capabilities'] = json.loads(str(self.capabilities))
-        return json.dumps(attr)
+        return str(self.__dict__)
 
+class HueLight(HueApiObject):
+    ROUTE = '/lights'
 
-def hue_get_lights(username):
-    json_response = requests.get(base_url + '/lights').json()
+    def __init__(self, bridge, id, attr):
+        super().__init__(bridge, id, attr)
 
-    lights = list()
-    for key,value in json_response.items():
-        light_capabilities = HueLightCapabilites(value['type'], value['capabilities'])
-        light_state = HueLightState(value['state']['on'], value['state'])
-        lights.append(HueLight(key, value['uniqueid'], value['modelid'], value['name'], value['type'], light_capabilities, light_state))
+        light_type = HUE_LIGHT_TYPES.get(self.type)
+        if light_type != None:
+            self.dimmable = light_type['dimmable']
+            self.supports_color = light_type['supports_color']
+            self.supports_ct = light_type['supports_ct']
+            
 
-    return lights
+    def update_state(self, new_state, transitiontime=HUE_DEFAULT_TRANSITION_TIME):
+        relative_url = self.ROUTE + '/' + '10' + '/state'
 
-hue_bridge_ip = "192.168.0.100"
-username = "x5lCjhRO0qBi3rf4MGVh-5pSEPq5vcnKpCGV0O9q"
-base_url = "http://" + hue_bridge_ip + "/api/" + username
+        result = dict()
+        for response in self.bridge._request('PUT', relative_url, new_state).json():
+            response_type, response_val = response.popitem()
 
-lights = hue_get_lights(username)
-current_light = None
-for light in lights: 
-    if light.id == 10: current_light = light
-
-current_light.blink()
+            if response_type == 'success': 
+                attr_name, attr_val = response_val.popitem()
+                attr_name = attr_name[len(relative_url+'/'):]
+                result[attr_name] = {'success':True, 'val':attr_val}
+                self.state.update({attr_name:attr_val})
+            elif response_type == 'error':
+                attr_name = response_val['address'][len(relative_url+'/'):]
+                result[attr_name] = {'success':False}
+        return result
+        
+class HueSensor(HueApiObject):
+    ROUTE = '/sensors'
